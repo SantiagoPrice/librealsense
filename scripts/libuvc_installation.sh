368f399d8a754a024e235b6af44cd5181d89ca2f
SantiagoPrice <santiago@ai.iit.tsukuba.ac.jp>
2025-06-13 04:00:38 +0000

    repo clone during libuvs install was removed

--- scripts/libuvc_installation.sh
+++ scripts/libuvc_installation.sh
@@ -16,9 +16,8 @@ fi
 lsb_release -a
 echo "Kernel version $(uname -r)"
 sudo apt-get update
-cd ~/
-sudo rm -rf ./librealsense_build
-mkdir librealsense_build && cd librealsense_build
+
+cd ../..
 
 if [ $(sudo swapon --show | wc -l) -eq 0 ];
 then
@@ -32,18 +31,13 @@ fi
 
 echo Installing Librealsense-required dev packages
 sudo apt-get install git cmake libssl-dev freeglut3-dev libusb-1.0-0-dev pkg-config libgtk-3-dev unzip -y
-rm -f ./master.zip
-
-wget https://github.com/IntelRealSense/librealsense/archive/master.zip
-unzip ./master.zip -d .
-cd ./librealsense-master
 
 echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
 sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
-cmake ../ -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=release -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true -DBUILD_WITH_CUDA=on
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr/local/realsen -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=release -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true -DBUILD_WITH_CUDA=on -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc
 make -j2
 sudo make install
 echo -e "\e[92m\n\e[1mLibrealsense script completed.\n\e[0m"
